9e16cf6f9464addaf9dc087c0504ad28dd5af8d2
Marek Muratow <contact@entito.pl>
2018-11-10 20:05:39 +0100

    init

---
+++ src/main/java/pl/entito/emailsender/SpringBoot2EmailSenderApplication.java
@@ -0,0 +1,12 @@
+package pl.entito.emailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2EmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2EmailSenderApplication.class, args);
+	}
+}
