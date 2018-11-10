9794a5533b35b14fceb8afa7c6f29b577d1ce0d3
Marek Muratow <contact@entito.pl>
2018-11-10 22:30:30 +0100

    sending functionality implemented

--- src/main/java/pl/entito/emailsender/SpringBoot2EmailSenderApplication.java
+++ src/main/java/pl/entito/emailsender/SpringBoot2EmailSenderApplication.java
@@ -1,12 +1,26 @@
 package pl.entito.emailsender;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot2EmailSenderApplication {
+public class SpringBoot2EmailSenderApplication implements CommandLineRunner {
+
+	@Autowired
+	private EmailSenderService sender;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot2EmailSenderApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String mailTo = "recipient@gmail.com";
+		String subject = "Email subject";
+		String msg = "Email body";
+
+		sender.sendSimpleMessage(mailTo, subject, msg);
 	}
 }
